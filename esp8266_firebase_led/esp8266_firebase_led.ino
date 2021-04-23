de00d55da559f7155ab6195849fda96e98ffca84
imvickykumar999 <imvickykumar999@gmail.com>
2021-04-23 23:03:21 +0530

    seperate folders

--- esp8266_firebase_led/esp8266_firebase_led.ino
+++ esp8266_firebase_led/esp8266_firebase_led.ino
@@ -2,37 +2,37 @@
 
 #include <ESP8266WiFi.h>
 #include "FirebaseESP8266.h"
-//#include <Servo.h> 
+//#include <Servo.h>
 
-int servoPin = 2; 
-//Servo Servo1; 
+int servoPin = 2;
+//Servo Servo1;
 
 #define WIFI_SSID "Vicky"
-#define WIFI_PASSWORD "oyevicks"
+#define WIFI_PASSWORD "*******"
 
 #define FIREBASE_HOST "led-blink-wifi-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "VvFhb5Ij53hPmECwjzf3lxmtXXUA7a0SqW34CNSa"
+#define FIREBASE_AUTH "*****************************************"
 
 FirebaseData firebaseData;
 
 void setup() {
-//  Servo1.attach(servoPin); 
-  
+//  Servo1.attach(servoPin);
+
   Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
-  
+
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
     delay(300);
   }
-  
+
   Serial.println();
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
-  
+
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
 
@@ -43,7 +43,7 @@ void setup() {
 }
 
 void loop() {
-  
+
     if (Firebase.getInt(firebaseData,"/led1"))
     {
       int val2 = (firebaseData.intData());
@@ -54,19 +54,19 @@ void loop() {
         digitalWrite(servoPin, HIGH);
         Serial.println("LOW");
       }
-      
+
 //      if (0<=val2<=180)
 //      {
-//        Servo1.write(val2); 
+//        Servo1.write(val2);
 //        delay(1000);
 //      }
-//      
+//
 //      else{
 //        Serial.println("enter valid position");
 //      }
 
     }
-        
+
     delay(200);
   // put your main code here, to run repeatedly:
 }
